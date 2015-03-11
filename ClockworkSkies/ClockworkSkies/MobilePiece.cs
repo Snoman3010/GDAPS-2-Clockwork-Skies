955f6a685925f260bfc00eb2d1d7f63dabce97e8
William Allen <snoman3010@gmail.com>
2015-03-11 10:45:24 -0400

    Started Sprite, Piece and MobilePiece classes

---
+++ ClockworkSkies/ClockworkSkies/MobilePiece.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClockworkSkies
+{
+    abstract class MobilePiece:Piece
+    {
+        private int direction;
+    }
+}
---
+++ ClockworkSkies/ClockworkSkies/Sprite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace ClockworkSkies
+{
+    class Sprite
+    {
+        private Rectangle spriteBox;
+        private Texture2D image;
+    }
+}
