f7f8d694bf96a136564341ca4318f8e73ccfe878
Erin McAnany <elm6208@rit.edu>
2015-03-18 11:01:30 -0400

    Updated the Button class

--- ClockworkSkies/ClockworkSkies/Button.cs
+++ ClockworkSkies/ClockworkSkies/Button.cs
@@ -19,17 +19,25 @@ namespace ClockworkSkies
         bool clicked;
 
         // constructor
-        public Button(Rectangle r, string txt, bool clckd)
+        public Button(Rectangle r, string txt)
         {
             rect = r;
             text = txt;
-            clicked = clckd;
+            clicked = false;
         }
 
         // Update method
-        public void Update()
+        public void Update(MouseState mState)
         {
-
+            // checks to see if the mouse location is inside the button
+            if(mState.X >= rect.X && mState.X <= rect.X + rect.Width && mState.Y >= rect.Y && mState.Y <= rect.Y + rect.Height)
+            {
+                // checks if the left mouse button is pressed and if it is, sets clicked to true
+                if (mState.LeftButton == ButtonState.Pressed)
+                {
+                    clicked = true;
+                }
+            }
         }
     }
 }
