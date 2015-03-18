696c3b59a8900c25cd32660b9ebcdabe26e600f8
William Allen <snoman3010@gmail.com>
2015-03-18 10:37:30 -0400

    Added level filestructure and level detection algorithm

---
+++ ClockworkSkies/ClockworkSkies/LevelList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ClockworkSkies
+{
+    class LevelList
+    {
+        private List<string> levels;
+
+        //Find level files
+        public void GetLevels()
+        {
+            try
+            {
+                //Level filenames should be CSkiesL#
+                //If there are holes in the sequence the later levels won't be found
+                //Looks for levels in an infinite loop until file isn't found
+                for (int i = 1; i > 0; i++)
+                {
+                    StreamReader reader = new StreamReader("Levels/CSkiesL" + i + ".txt");
+                    levels.Add("Levels/CSkiesL" + i + ".txt");
+                    reader.Close();
+                }
+            }
+            catch (FileNotFoundException fnf)
+            {
+                return;
+            }
+        }
+    }
+}
